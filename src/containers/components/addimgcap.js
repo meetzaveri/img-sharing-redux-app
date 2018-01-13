3621af26251d5fb2475a2e2e157b1ed6d7357a8b
Meet Zaveri <meethpatel96@gmail.com>
2018-01-13 16:04:27 +0530

    Update addimgcap.js

--- src/containers/components/addimgcap.js
+++ src/containers/components/addimgcap.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-
 const InputElement = ({
     uploadcontent
 }) => {
@@ -11,19 +9,23 @@ const InputElement = ({
     return(
         <div>
             <h2> Add Image URL and caption :</h2>
-                <input
-                style ={{ margin : '0 15px'}}
-                ref={node => {
-                    inputImg = node;
-                }}/>
-                <input 
-                style ={{ margin : '0 15px'}}
-                ref={node => {
-                    inputCap = node;
-                }}
-                placeholder ="Image Caption"
-                />
-            <button onClick={() => {
+                <div className="ui input focus">
+                    <input
+                    style ={{ margin : '0 15px'}}
+                    ref={node => {
+                        inputImg = node;
+                    }}
+                    placeholder ="Image URL"
+                    />
+                    <input 
+                    style ={{ margin : '0 15px'}}
+                    ref={node => {
+                        inputCap = node;
+                    }}
+                    placeholder ="Image Caption"
+                    />
+                </div>
+            <button className="ui button" onClick={() => {
                 uploadcontent(inputImg.value,inputCap.value);
                 inputImg.value = '';
                 inputCap.value = '';
